a83b8ce7ca52cf4c02c216decf2c041ba5620d10
khushboo1234 <khusboothakurcs48@gmail.com>
2015-06-13 13:50:29 +0530

    1 files added

---
+++ github.py
@@ -0,0 +1,15 @@
+# class Github(object):
+# 	def__init__(self,members,code):
+# 	   self.members = members
+# 	   self.code = code
+
+# class Dinesh-Sunny(Github):
+# 	def__init__():
+
+
+from random import *
+
+names = ["aditi","khushboo","nazish"]
+print( choice(names) )
+char = ['a','b','c','d']
+print(choice(char))
